0d534cc2605169802262ca1eb9f5d253f9e2c5a5
Fatbardh Berisha <fatbardhberisha@Laura.yallo.box>
2023-11-24 18:26:58 +0100

    nav-start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,68 @@
+<head>
+    <title>Stilix</title>
+    <meta name="description" content="Kreative Branding-Agentur, die Unternehmen durch erstklassiges Branding, innovatives Design und individuelle Strategien zum Erfolg verhilft. Erfahren Sie mehr und kontaktieren Sie uns jetzt!">
+	  <meta name="keywords" content="bisha, Branding-Agentur, Markenbildung, Design, Bisha Wallis, Markenstrategie, Logo-Design, Verpackungsdesign, Marketingmaterialien, Unternehmenserfolg" />
+    <link rel="canonical" href="https://stilix.ch/">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+
+<header>
+    <div class="logo">
+        <img src="images/stilix-logo.svg" alt="">
+    </div>
+    <nav>
+        <a href="/service">service</a>
+        <a href="/kontakt">kontakt</a>
+    </nav>
+</header>
+
+
+
+<slot></slot>
+
+<style>
+
+    :global(*){
+       padding: 0;
+       margin: 0;
+       margin: 0 auto;
+       box-sizing: border-box;
+       font-family: 'Poppins', sans-serif;
+    
+    }
+
+
+
+    :global(html){
+        font-size: 10px ;
+    }
+
+    header{
+        padding: 5rem 0rem 2rem 0rem;
+        width: 70%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    nav{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
+    }
+
+    a{
+        font-size: 24px;
+        color: #2b2d42;
+        text-decoration: none;
+        transition: 0.2s ease-out;
+    }
+
+    a:hover{
+        color: #ef233c;
+    }
+</style>
