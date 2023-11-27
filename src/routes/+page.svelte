47631c0d2d5bd4881e3553958df9438d127d1d51
zerokater <fatbardh.berisha@outlook.com>
2023-11-27 11:01:56 +0100

    headline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,29 +8,13 @@
 <main>
     <section>
 
-        <h1>Von der Gestaltung neuer<br>Marken bis hin zum kleinsten<br>Detail im Design</h1>
-        <PrimaryButton link="/kontakt">Kontaktiere uns</PrimaryButton>
-        <SecondaryButton link="/service">Service</SecondaryButton>
-    </section>
-</main>
+        <h1>Deine Vision</h1>
+        <h1>
+            unsere Kreation</h1>
 
-<main class="projekte">
-    <section>
-        <div class="projekt">
-            <img src="images/design.png" alt="">
-            <h2>Design</h2>
-        </div>
-        <div class="projekt">
-            <img src="images/kamera.png" alt="">
-            <h2>Foto</h2>
-        </div>
-        <div class="projekt">
-            <img src="images/videokamera.png" alt="">
-            <h2>Video</h2>
-        </div>
-        <div class="projekt">
-            <img src="images/sticker.png" alt="">
-            <h2>Sticker</h2>
+        <div class="buttons">
+            <PrimaryButton link="/kontakt">Kontaktiere uns</PrimaryButton>
+            <SecondaryButton link="/service">Service</SecondaryButton>
         </div>
     </section>
 </main>
@@ -56,44 +40,19 @@
 
     h1{
         font-size: 64px;
+        width: 95%;
         color: #2b2d42;
     }
 
 
-    .projekte{
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .projekte section{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .projekt{
-        border-radius: 2rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        gap: 3rem;
-        background-color: #ef233c;
-        width: 350px;
-        height: 350px;
-    }
-
-    .projekt img{
-        width: 96px;
-    }
-
-    .projekt h2{
-        font-size: 26px;
-        color: #efefef;
-    }
-
-
+    /* For small devices (phones, 580px and up) */
+    @media only screen and (max-width: 576px) { 
+        h1{
+            font-size: 32px;
+        }
+        section{
+            width: 90%;
+        }
+     }
 
 </style>
